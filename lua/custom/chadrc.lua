b61f422c28e14d683ef931d711994a2e284af096
pyk <gm@pyk.sh>
2023-10-27 11:19:38 +0700

    chore: add custom configs

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,15 @@
+---@type ChadrcConfig
+local M = {}
+
+M.ui = {
+  theme = 'yoru',
+  hl_override = {
+    Normal = {
+      bg = "#000000"
+    }
+  }
+}
+
+M.plugins = "custom.plugins"
+
+return M
---
+++ lua/custom/plugins.lua
@@ -0,0 +1,51 @@
+local plugins = {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "typescript-language-server",
+        "eslint-lsp",
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function ()
+      require "custom.configs.lint"
+    end
+  },
+  {
+     "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        -- defaults 
+        "vim",
+        "lua",
+
+        -- web dev 
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+        "tsx",
+        "json",
+        "astro",
+        -- "vue", "svelte",
+
+        -- low level
+        -- "c",
+        -- "zig"
+      },
+    }
+  }
+}
+
+return plugins
